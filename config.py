55a416f547ce2ea3285b7b38e86b83e08ec7034f
Haani Ali <haani.ali@saratoga-homes.com>
2025-08-06 14:21:12 -0500

    Add scraper.py and switch config.py to BASE_URLS list

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 # List of all community “Available Homes” URLs you want to scrape
 BASE_URLS = [
-    "https://www.drhorton.com/texas/austin/austin/trails-at-wildhorse#relatedmovein",
-    "https://www.drhorton.com/texas/austin/manor/monarch-ranch%20#relatedmovein",
-    "https://www.drhorton.com/texas/austin/manor/palomino%20#relatedmovein",
-    "https://www.drhorton.com/texas/austin/manor/carillon%20#relatedmovein",
-    "https://www.drhorton.com/texas/austin/mustang-ridge/durango#relatedmovein",
+    
     
     
     # add as many as you like here…
